bca507678da572b08a4d9237157eba62050089ff
tarek fouda <tarek.fouda@barqorders.com>
2023-01-12 13:57:52 +0200

    splash

--- lib/features/base/presentation/pages/splash/splash_imports.dart
+++ lib/features/base/presentation/pages/splash/splash_imports.dart
@@ -1,9 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tdd/core/constants/my_colors.dart';
 import 'package:flutter_tdd/core/helpers/init_custom_packages.dart';
 import 'package:flutter_tdd/core/routes/router_imports.gr.dart';
-import 'package:tf_custom_widgets/tf_custom_widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:widget_and_text_animator/widget_and_text_animator.dart';
 
 part 'splash.dart';
 part 'splash_controller.dart';
